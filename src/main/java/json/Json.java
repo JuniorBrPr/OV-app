d11cc7737c596b5daa325a46885b409a8c263653
JuniorBrPr <junior.britoperez@student.hu.nl>
2021-12-08 22:18:04 +0100

    Users are now stored in a json file and an user object has been created.

---
+++ src/main/java/json/Json.java
@@ -0,0 +1,58 @@
+package json;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.util.List;
+
+public class Json {
+    public void addUser(User user){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream inputStream = new FileInputStream(new File("src/main/resources/jsonFiles/users.json"));
+            var typeReference = new TypeReference<List<User>>() {};
+            List<User> users = mapper.readValue(inputStream,typeReference);
+//            int lastUserIdgetter = users.get(users.size()-1).getUserId();
+            user.setUserId(users.size());
+            users.add(user);
+            mapper.writeValue(new File("src/main/resources/jsonFiles/users.json"), users);
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int  login(String username, String password){
+        int userId = 0;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream inputStream = new FileInputStream(new File("src/main/resources/jsonFiles/users.json"));
+            var typeReference = new TypeReference<List<User>>() {};
+            List<User> users = mapper.readValue(inputStream,typeReference);
+            for (User u : users){
+                if(username.equals(u.getUserName()) && password.equals(u.getPassword())){
+                    userId = u.getUserId();
+                }
+            }
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userId;
+    }
+}
---
+++ src/main/java/json/User.java
@@ -0,0 +1,42 @@
+package json;
+
+import java.util.ArrayList;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String password;
+//    private Trip[] trips;
+//
+//    public Trip[] getTrips() {
+//        return trips;
+//    }
+//
+//    public void setTrips(Trip[] trips) {
+//        this.trips = trips;
+//    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
